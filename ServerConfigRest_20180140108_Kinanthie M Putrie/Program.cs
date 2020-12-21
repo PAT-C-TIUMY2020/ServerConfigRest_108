45731bda06a160e4b404cd393adbbea519157f04
KinanthieMPutrie <kinanthieputrie@gmail.com>
2020-12-21 23:10:30 +0700

    Add project files.

---
+++ ServerConfigRest_20180140108_Kinanthie M Putrie/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServerRest_20180140108_Kinanthie_M_Putrie;
+
+namespace ServerConfigRest_20180140108_Kinanthie_M_Putrie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server ready ..");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
